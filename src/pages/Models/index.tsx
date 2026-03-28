f55eaca2986ac372a4148868035430b1416108b9
ashione <skyzlxuan@gmail.com>
2026-03-28 20:57:56 +0800

    remove unused i18next type import

--- src/pages/Models/index.tsx
+++ src/pages/Models/index.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useReducer, useRef, useState } from 'react';
-import type { TFunction } from 'i18next';
 import { useTranslation } from 'react-i18next';
 import {
   ChevronLeft,
